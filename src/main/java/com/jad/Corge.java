85f30d88c6ddde01a346096254749a1602cac095
Tur Jean-Baptiste <jean-baptiste.tur@student.junia.com>
2025-09-18 12:21:45 +0200

    Classes completed

--- src/main/java/com/jad/Corge.java
+++ src/main/java/com/jad/Corge.java
@@ -1,5 +1,18 @@
 package com.jad;
 
 public class Corge {
+    private Foo foo;
+
+    public Corge(Foo foo) {
+        this.foo = foo;
+    }
+
+    public Foo getFoo() {
+        return foo;
+    }
+
+    public void setFoo(Foo foo) {
+        this.foo = foo;
+    }
 
 }
--- src/main/java/com/jad/Foo.java
+++ src/main/java/com/jad/Foo.java
@@ -6,4 +6,60 @@ public class Foo {
     private Qux qux;
     private Corge corge;
     private Grault[] grauls;
+
+    public Foo(Bar bar, Baz[] bazs, Qux qux, Corge corge, Grault[] grauls) {
+        this.bar = bar;
+        this.bazs = bazs;
+        this.qux = qux;
+        this.corge = corge;
+        this.grauls = grauls;
+    }
+
+    public void addBaz(Baz baz) {
+
+    }
+
+    public void addGrault() {
+
+    }
+
+    public Bar getBar() {
+        return this.bar;
+    }
+
+    public Baz[] getBazs() {
+        return this.bazs;
+    }
+
+    public Qux getQux() {
+        return this.qux;
+    }
+
+    public Corge getCorge() {
+        return this.corge;
+    }
+
+    public Grault[] getGrauls() {
+        return this.grauls;
+    }
+
+    public void setBar(Bar bar) {
+        this.bar = bar;
+    }
+
+    public void setBazs(Baz[] bazs) {
+        this.bazs = bazs;
+    }
+
+    public void setQux(Qux qux) {
+        this.qux = qux;
+    }
+
+    public void setCorge(Corge corge) {
+        this.corge = corge;
+    }
+
+    public void setGrauls(Grault[] grauls) {
+        this.grauls = grauls;
+    }
 }
